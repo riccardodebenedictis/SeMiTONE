52271a460953e9557e1aa649aba6c11f7ae36781
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2018-04-12 19:29:06 +0200

    minor cleanings..

--- src/main/java/it/cnr/istc/pst/semitone/pb/PBTheory.java
+++ src/main/java/it/cnr/istc/pst/semitone/pb/PBTheory.java
@@ -32,14 +32,12 @@ import it.cnr.istc.pst.semitone.sat.Lit;
 import it.cnr.istc.pst.semitone.sat.Sat;
 import it.cnr.istc.pst.semitone.sat.Theory;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap.Entry;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 
 /**
  *
@@ -249,15 +247,6 @@ public class PBTheory implements Theory {
                             }
                         }
                         sat.record(learnt_clause.toArray(new Lit[learnt_clause.size()]));
-                        for (Entry<Rational> var : c.expr.vars.int2ObjectEntrySet()) {
-                            c_watches.get(var.getIntKey()).remove(c);
-                            if (!layers.isEmpty()) {
-                                layers.peekFirst().cnstrs.add(c);
-                            }
-                            if (c_watches.get(var.getIntKey()).isEmpty()) {
-                                c_watches.remove(var.getIntKey());
-                            }
-                        }
                     } else if (c.known_term.leq(c.lb)) {
                         // the constraint cannot be satisfied..
                         final List<Lit> learnt_clause = new ObjectArrayList<>();
@@ -273,15 +262,6 @@ public class PBTheory implements Theory {
                             }
                         }
                         sat.record(learnt_clause.toArray(new Lit[learnt_clause.size()]));
-                        for (Entry<Rational> var : c.expr.vars.int2ObjectEntrySet()) {
-                            c_watches.get(var.getIntKey()).remove(c);
-                            if (!layers.isEmpty()) {
-                                layers.peekFirst().cnstrs.add(c);
-                            }
-                            if (c_watches.get(var.getIntKey()).isEmpty()) {
-                                c_watches.remove(var.getIntKey());
-                            }
-                        }
                     }
                     break;
                 }
@@ -308,15 +288,6 @@ public class PBTheory implements Theory {
                             }
                         }
                         sat.record(learnt_clause.toArray(new Lit[learnt_clause.size()]));
-                        for (Entry<Rational> var : c.expr.vars.int2ObjectEntrySet()) {
-                            c_watches.get(var.getIntKey()).remove(c);
-                            if (!layers.isEmpty()) {
-                                layers.peekFirst().cnstrs.add(c);
-                            }
-                            if (c_watches.get(var.getIntKey()).isEmpty()) {
-                                c_watches.remove(var.getIntKey());
-                            }
-                        }
                     } else if (c.known_term.geq(c.ub)) {
                         // the constraint is already satisfied..
                         final List<Lit> learnt_clause = new ObjectArrayList<>();
@@ -332,15 +303,6 @@ public class PBTheory implements Theory {
                             }
                         }
                         sat.record(learnt_clause.toArray(new Lit[learnt_clause.size()]));
-                        for (Entry<Rational> var : c.expr.vars.int2ObjectEntrySet()) {
-                            c_watches.get(var.getIntKey()).remove(c);
-                            if (!layers.isEmpty()) {
-                                layers.peekFirst().cnstrs.add(c);
-                            }
-                            if (c_watches.get(var.getIntKey()).isEmpty()) {
-                                c_watches.remove(var.getIntKey());
-                            }
-                        }
                     }
                     break;
                 }
@@ -364,28 +326,10 @@ public class PBTheory implements Theory {
     @Override
     public void pop() {
         Layer layer = layers.pollFirst();
-        // we decrease the lower bounds..
-        for (java.util.Map.Entry<PBConstraint, Rational> lb : layer.lbs.entrySet()) {
-            if (lb.getValue().isPositive()) {
-                lb.getKey().lb.add(lb.getValue());
-            } else {
-                lb.getKey().lb.sub(lb.getValue());
-            }
-        }
-        // we increase the upper bounds..
-        for (java.util.Map.Entry<PBConstraint, Rational> ub : layer.ubs.entrySet()) {
-            if (ub.getValue().isPositive()) {
-                ub.getKey().ub.sub(ub.getValue());
-            } else {
-                ub.getKey().ub.add(ub.getValue());
-            }
-        }
-        // we restore the removed constraints..
-        for (PBConstraint c : layer.cnstrs) {
-            for (Entry<Rational> var : c.expr.vars.int2ObjectEntrySet()) {
-                c_watches.computeIfAbsent(var.getIntKey(), k -> new ObjectArrayList<>()).add(c);
-            }
-        }
+        // we restore the lower bounds..
+        layer.lbs.entrySet().forEach(lb -> lb.getKey().lb = lb.getValue());
+        // we restore the upper bounds..
+        layer.ubs.entrySet().forEach(ub -> ub.getKey().ub = ub.getValue());
     }
 
     private class PBConstraint {
@@ -394,16 +338,15 @@ public class PBTheory implements Theory {
         final Lin expr;
         final Op op;
         final InfRational known_term;
-        final Rational lb, ub;
+        Rational lb, ub;
 
         PBConstraint(int b, final Lin expr, Rational lb, Rational ub, final Op op, InfRational known_term) {
             this.b = b;
             this.expr = expr;
             this.op = op;
             this.known_term = known_term;
-            for (Entry<Rational> var : expr.vars.int2ObjectEntrySet()) {
-                c_watches.computeIfAbsent(var.getIntKey(), k -> new ObjectArrayList<>()).add(this);
-            }
+            expr.vars.int2ObjectEntrySet()
+                    .forEach(var -> c_watches.computeIfAbsent(var.getIntKey(), k -> new ObjectArrayList<>()).add(this));
             this.lb = lb;
             this.ub = ub;
         }
@@ -435,6 +378,5 @@ public class PBTheory implements Theory {
     private static class Layer {
         private final Object2ObjectMap<PBConstraint, Rational> lbs = new Object2ObjectOpenHashMap<>();
         private final Object2ObjectMap<PBConstraint, Rational> ubs = new Object2ObjectOpenHashMap<>();
-        private final ObjectOpenHashSet<PBConstraint> cnstrs = new ObjectOpenHashSet<>();
     }
 }
